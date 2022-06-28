49af36492df5dfa34541f9c51c783b00896f05ec
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-06-28 11:53:58 -0700

    set up workspace, added routes and state variables in app.jsx

--- bank-of-codepath-ui/src/components/App/App.jsx
+++ bank-of-codepath-ui/src/components/App/App.jsx
@@ -1,13 +1,31 @@
-import * as React from "react"
-import Navbar from "../Navbar/Navbar"
-import Home from "../Home/Home"
-import "./App.css"
+import * as React from "react";
+import Navbar from "../Navbar/Navbar";
+import Home from "../Home/Home";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { TransactionDetail } from "../TransactionDetail/TransactionDetail";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Home />
-    </div>
-  )
+
+  // state variables and their handlers
+  const [isLoading, setIsLoading] = useState(false);
+  const [transactions, setTransactions] = useState([]);
+  const [transfers, setTransfers] = useState([]);
+  const [error, setError] = useState();
+  const [filterInputValue, setFilterInputValue] = useState("");
+
+	return (
+		<div className='App'>
+			<BrowserRouter className='app'>
+				<Navbar />
+				<main>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/transactions/:transactionId' element={<TransactionDetail />} />
+					</Routes>
+				</main>
+			</BrowserRouter>
+		</div>
+	);
 }
